1cff28a7bbb7e97eb47699bb9dc5a5763176e73c
SIRREG001 <udochukwureginald78@gmail.com>
2023-09-08 02:17:50 +0100

    Create logic for the app

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const calculateBtn = document.getElementById("calculate-btn");
 const resultDiv = document.getElementById("result");
 
 calculateBtn.addEventListener("click", function () {
-    const dob = new Date(dobInput.value)
-    const ageInMins = Date.now() - dob.getTime()
-    const ageDate = new Date(ageInMins)
+    const dob = new Date(dobInput.value);
+    const ageInMins = Date.now() - dob.getTime();
+    const ageDate = new Date(ageInMins);
     const age = Math.abs(ageDate.getUTCFullYear() - 1970);
 
     resultDiv.innerHTML = `Your Age is ${age} years.`
